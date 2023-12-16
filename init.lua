4ddee39afe72a2cdeea6851b67b7467f92f2a960
Lucas Veado <lfurtini@gmail.com>
2023-12-17 00:07:00 +0100

    Commenting c sharp lsp for now

--- init.lua
+++ init.lua
@@ -575,7 +575,7 @@ local servers = {
   -- rust_analyzer = {},
   tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
-  csharp_ls = {},
+  -- csharp_ls = {},
 
   lua_ls = {
     Lua = {
