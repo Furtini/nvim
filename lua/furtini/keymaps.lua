b143f455eb0cd5287b25871149287c31c6cfafd5
Lucas Veado <lfurtini@gmail.com>
2023-12-29 20:20:13 +0100

    Add new keymaps

--- lua/furtini/keymaps.lua
+++ lua/furtini/keymaps.lua
@@ -10,6 +10,39 @@ vim.keymap.set('n', '<leader>sp', vim.cmd.Ex, { desc = '[S]earch [P]roject' })
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
+-- In visual mode, J and K move selection up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Make cursor stay in place when using J
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep cursor in middle of screen when jumping lines
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Make cursor stay in middle when searching
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Preserve copied text when pasting over highlighted text
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- Copy to system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+-- Delete selection to void
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- Avoid accidental quit on Q
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Open projects via tmux-sessionizer
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
